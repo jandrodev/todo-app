5db3821197979a4992d215d7aa8518e55131e7ad
Jandro Seisdedos <5932584+jandrodev@users.noreply.github.com>
2023-07-13 12:28:04 +0200

    Create frontend structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+
+// Quasar config
+import { Quasar } from 'quasar';
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true});
+        return pages[`./Pages/${name}.vue`];
+    },
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+            .use(plugin)
+            .use(Quasar)
+            .mount(el)
+    },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { quasar } from '@quasar/vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,21 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        quasar({
+            sassVariables: 'resources/css/quasar-variables.sass',
+        }),
     ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
